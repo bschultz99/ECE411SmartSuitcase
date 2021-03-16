3b09af6284b8f482ed00253446424870f207ecf7
schlutz <bryantschultz99@gmail.com>
2021-03-16 15:08:33 -0500

    Sensor code

---
+++ Arduino/EnvironmentalSensors/EnvironmentalSensors.ino
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <SPI.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BME280.h>
+#include "Adafruit_VEML6070.h"
+#include "Adafruit_SGP30.h"
+
+#define BME_SCK 13
+#define BME_MISO 12
+#define BME_MOSI 11
+#define BME_CS 10
+
+#define SEALEVELPRESSURE_HPA (1013.25)
+
+Adafruit_VEML6070 uv = Adafruit_VEML6070();
+Adafruit_BME280 bme //I2C connection
+
+unsigned long delayTime;
+
+void setup() {
+
+}
+
+
+void loop() {
+
+}
+
+uint32_t getAbsoluteHumidity(float temperature, float humidity) {
+    const float absoluteHumidity = 216.7f * ((humidity / 100.0f) * 6.112f * exp((17.62f * temperature) / (243.12f + temperature)) / (273.15f + temperature)); // [g/m^3]
+    const uint32_t absoluteHumidityScaled = static_cast<uint32_t>(1000.0f * absoluteHumidity); // [mg/m^3]
+    return absoluteHumidityScaled;
+}
