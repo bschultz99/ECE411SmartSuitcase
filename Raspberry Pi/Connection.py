2d0de8aace447e8beb29dc1d4543b4b6056696c1
Bryant <bryantschultz99@gmail.com>
2021-03-25 11:21:21 -0500

    Sensor code updates

--- Raspberry Pi/Connection.py
+++ Raspberry Pi/Connection.py
@@ -2,10 +2,10 @@
 import serial
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.flush()
+    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1) #Set the serial port to be ttyUSB0 at a baudrate of 9600
+    ser.flush() #Wait till data is written
 
     while True:
         if ser.in_waiting > 0:
-            line = ser.readline().decode('latin-1').rstrip()
+            line = ser.readline().decode('latin-1').rstrip() #Read
             print(line)
