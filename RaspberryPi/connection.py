5c78d941d10ecf8e6889da865c46ec56e2acd04e
schlutz <bryantschultz99@gmail.com>
2021-04-15 19:25:32 -0500

    connection testing

--- RaspberryPi/connection.py
+++ RaspberryPi/connection.py
@@ -2,10 +2,19 @@
 import serial
 
 if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
-    ser.flush()
+    ser1 = serial.Serial('/dev/ttyUSB0', 9600, timeout=1)
+    ser2 = serial.Serial('/dev/rfcomm0')
+    ser1.flush()
 
     while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('latin-1').rstrip()
-            print(line)
+        #Communication from Arduino to Pi
+        if ser1.in_waiting > 0:
+            arduino = ser1.readline().decode('utf-8').rstrip()
+            #print(arduino)
+        #Communication from Phone to Pi
+        if ser2.in_waiting > 0:
+            phone = ser2.readline().decode('utf-8').rstrip()
+            #print(phone)
+        
+
+        
